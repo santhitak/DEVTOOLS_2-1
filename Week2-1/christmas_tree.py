cc38b4c475374ee39d2870f9d130c16090a0de0b
santhitak <63070175@kmitl.ac.th>
2023-01-24 14:50:40 +0700

    feat: christmas tree

--- Week2-1/christmas_tree.py
+++ Week2-1/christmas_tree.py
@@ -1,9 +1,7 @@
 def draw_tree(height):
   for i in range(1, height + 1):
-    for j in range(height - i):
-      print(" ", end="")
-    for j in range(2 * i - 1):
-      print("*", end="")
+    for j in range(height - i): print(" ", end="")
+    for j in range(2 * i - 1): print("*", end="")
     print()
 
 draw_tree(10)
